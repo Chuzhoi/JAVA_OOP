a159e16dfb6de463286115970d98a220c5a4f566
Konstantin <parnishkafin@gmail.com>
2023-03-21 19:54:43 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,13 @@
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String phone;
+    public String passport;
+
+    //public String birthday;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
+
